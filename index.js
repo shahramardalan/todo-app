33b66261fe9fa5289261b6710d2ab53f07395f87
shahram <ardalan.shahram@gmail.com>
2023-10-10 15:38:47 +0330

    add list to the localStorage

--- index.js
+++ index.js
@@ -6,16 +6,33 @@ const list = [];
 
 function syncStorage(item) {
   list.push(item);
+  const nextList = JSON.stringify(list);
+  localStorage.setItem("todo_list", nextList);
+}
+
+function createItem(item) {
+  todoList.innerHTML += `
+  <div class="list">
+  <input type="checkbox" name="" id="" />
+  <span>${item}</span>  
+  </div>
+  `;
+  syncStorage(result);
 }
 
 saveBtn.addEventListener("click", () => {
   result = todoInput.value;
-  todoList.innerHTML += `
-  <div class="list">
-  <input type="checkbox" name="" id="" />
-  ${result}
-</div>
-  `;
-  syncStorage(result);
-  console.log(list);
+  if (result === "") {
+    alert("todo is empty!!!");
+  } else {
+    createItem(result);
+    todoInput.value = "";
+  }
 });
+
+const previousList = JSON.parse(localStorage.getItem("todo_list"))
+
+for(let i=0; i < previousList.length; i++){
+  const title = previousList[i];
+  createItem(title)
+}
