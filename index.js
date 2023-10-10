246a755a8beceae59f638dbdf5c9ca2459d273bb
Shahram <ardalan.shahram@gmail.com>
2023-10-10 13:06:47 +0330

    add syncStorage function and add event listener on save button

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const saveBtn = document.querySelector("#saveBtn");
 const todoInput = document.querySelector("#todoInp");
+const todoList = document.querySelector("#list");
+let result = "";
+const list = [];
+
+function syncStorage(item) {
+  list.push(item);
+}
 
 saveBtn.addEventListener("click", () => {
-  const result = todoInput.value;
-  console.log(result);
+  result = todoInput.value;
+  todoList.innerHTML += `
+  <div class="list">
+  <input type="checkbox" name="" id="" />
+  ${result}
+</div>
+  `;
+  syncStorage(result);
+  console.log(list);
 });
