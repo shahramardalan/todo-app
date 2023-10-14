82104943449b13b662409e67a25bcf9482243224
shahram <ardalan.shahram@gmail.com>
2023-10-15 00:04:05 +0330

    add change statuse and delete and filter functionality

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const saveBtn = document.querySelector("#saveBtn");
 const todoInput = document.querySelector("#todoInp");
 const todoList = document.querySelector("#list");
-let link = ""
+const todoFilter = document.querySelector("#filter");
+
 let list = [];
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 function init() {
   loadFromStorage();
   renderList();
-  link = document.querySelector('[data-target="8"]');
-  
 }
 
 function syncStorage(item) {
   const next_item = {
-    id: item.id,
+    target: item.target,
     title: item.title,
     status: item.status,
   };
   list.push(next_item);
+  const nextList = JSON.stringify(list);
+  localStorage.setItem("todo_list", nextList);
+}
+
+function updateStorage(item) {
+  const next_item = {
+    target: item.target,
+    title: item.title,
+    status: item.status,
+  };
   const nextList = JSON.stringify(list);
   localStorage.setItem("todo_list", nextList);
 }
@@ -33,11 +50,21 @@ function renderList() {
     renderItem(item);
   }
 }
+
 function renderItem(item) {
   todoList.innerHTML += `
   <div class="list">
-  <input type="checkbox" ${item.status ? "checked" : ""} />
-  <span data-target="${item.id}">${item.title}</span>  
+  <div>
+    <input data-action="change" data-target="${item.target}" type="checkbox" ${
+    item.status ? "checked" : ""
+  } />
+    <span data-target="${item.target}">${item.title}</span>  
+  </div>
+  <div>
+    <button data-action="delete" data-target="${
+      item.target
+    }" class="delete">Delete Todo</button>
+  </div>
   </div>
   `;
 }
@@ -48,11 +75,12 @@ function clearInput() {
 
 function onAddItem() {
   const result = todoInput.value;
+  const target = uuidv4();
   if (result === "") {
     alert("todo is empty!!!");
   } else {
     const item = {
-      id : list.length,
+      target: target,
       title: result,
       status: false,
     };
@@ -64,5 +92,47 @@ function onAddItem() {
 
 saveBtn.addEventListener("click", onAddItem);
 
+function onChangeStatus(item) {
+  const target = item.target;
+  if (target.dataset.action === "change") {
+    const currentTodoTarget = target.dataset.target;
+    const item = list.filter((item) => item.target === currentTodoTarget);
+    if (item[0].status === false) {
+      item[0].status = true;
+    } else {
+      item[0].status = false;
+    }
+    updateStorage(item);
+  }
+}
+
+todoList.addEventListener("click", onChangeStatus);
+
+function onDeleteItem(item) {
+  const target = item.target;
+  if (target.dataset.action === "delete") {
+    const currentTodoTarget = target.dataset.target;
+    const item = list.filter((item) => item.target === currentTodoTarget);
+    const index = list.indexOf(item[0]);
+    list.splice(index, 1);
+  }
+  const nextList = JSON.stringify(list);
+  localStorage.setItem("todo_list", nextList);
+  location.reload();
+}
+
+todoList.addEventListener("click", onDeleteItem);
+
+todoFilter.addEventListener("change", (event) => {
+  if (event.target.value === 'done') {
+    const filtered = list.filter((item) => item.status === true);
+    console.log(filtered);
+  }else if(event.target.value === 'todo'){
+    const filtered = list.filter((item) => item.status === false);
+    console.log(filtered);
+  }else if(event.target.value === 'all'){
+    const filtered = list.filter((item) => item.status === false || item.status === true);
+    console.log(filtered);
+  }
+});
 init();
-alert(link.innerHTML)
